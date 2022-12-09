4c65de84873e3b41d46d2814bbee8cbd5a05b0c2
Grayson Hansard <grayson.hansard@bitrise.io>
2022-12-09 10:58:41 -0500

    Run swiftlint with and without GHA reporter

--- bin/do_lint.sh
+++ bin/do_lint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 BIN_DIR="$(dirname $(echo "$BASH_SOURCE[0]"))"
-$BIN_DIR/swiftlint lint --reporter github-actions-logging
+$BIN_DIR/swiftlint lint $@
+# Pass in: --reporter github-actions-logging
