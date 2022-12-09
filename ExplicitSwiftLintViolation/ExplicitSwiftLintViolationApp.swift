589af7e1b93eb5fe723ef84bf6bfe0dc4f9274d7
Grayson Hansard <grayson.hansard@bitrise.io>
2022-12-09 10:28:34 -0500

    Initial Commit

---
+++ ExplicitSwiftLintViolation/ExplicitSwiftLintViolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplicitSwiftLintViolationApp.swift
+//  ExplicitSwiftLintViolation
+//
+//  Created by Grayson Hansard on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ExplicitSwiftLintViolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
